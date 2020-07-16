1eed962c11182abdc5f1a090036938979f0228bc
Zaid Salman <58643601+zaidsalman497@users.noreply.github.com>
2020-07-16 21:22:11 +1000

    I updated login page

---
+++ index.js
@@ -0,0 +1,19 @@
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function countDown() {
+    let currentVal = document.getElementById("countDownButton").innerHTML;
+    let newVal = 0;
+    if(currentVal > 0) {
+        newVal = currentVal - 1;
+    }
+    document.getElementById("countDownButton").innerHTML = newVal;
+}
+
+
+//function checkUserLogin() {
+    // setTimeout(() => {
+    //     window.location.href = "login.html";
+    // }, 1000);
+}

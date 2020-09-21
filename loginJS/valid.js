b315c18a5b121fa6654401bf57702529cd8995bc
Zaid Salman <58643601+zaidsalman497@users.noreply.github.com>
2020-09-21 10:33:37 +1000

    add login form website

--- loginJS/valid.js
+++ loginJS/valid.js
@@ -4,6 +4,9 @@ var password = document.forms['form']['password']
 var email_error = document.getElementById('email_error');
 var pass_error = document.getElementById('pass_error');
 
+email.addEventListener('textInput',email_Verify);
+password.addEventListener('textInput',password_Verify);
+
 function validated(){
     if (email.value.length < 9) {
         email.style.border = "1px solid red";
@@ -11,4 +14,29 @@ function validated(){
         email.focus();
         return false;
     }
+    if (password.value.length < 6) {
+        password.style.border = "1px solid red";
+        password_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+
+
+    
 }
+
+
+function email_Verify(){
+    if (email.value.length >= 8)
+    email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+}
+
+function password_Verify(){
+    if (password.value.length >= 8)
+    password.style.border = "1px solid silver";
+        password_error.style.display = "none";
+        return true;
+}
+

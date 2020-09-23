e0c6ab1b41b7b406576c3d416c990df300a5b5d6
Zaid Salman <58643601+zaidsalman497@users.noreply.github.com>
2020-09-23 12:02:40 +1000

    login.js and style.css  not working

--- loginJS/login.js
+++ loginJS/login.js
@@ -1,24 +1,52 @@
 class AppForm {
     constructor() {
         this.Form = [];
-        this.step = 0;
+        this.step = 1;
         this.currentGroup = null;
 
         this.setListener();
+        this.getForm();
+        this.refresh();
     }
 
     refresh = () => {
-        console.log('refreshing');
         this.step++;
+        this.displayStep();
     }
 
+    displayStep = () => {
+        if(this.currentGroup)
+        this.currentGroup.style.display = 'none';
+        this.currentGroup = this.form.find(_group => _group.step === this.step);
+        this.currentGroup.style.display = 'block'
+    }
 
+    getForm = () => {
+        const groups = Array.from(document.getElementsByClassName('form-group'));
+        groups.forEach(_group => {
+            var children = Array.from(_group.children)
+            this.form.push({
+                'step': Number.parseInt(_group.dataset.step),
+                'element': _group,
+                'input': children.find(_element => _element.nodeName === 'INPUT'),
+            });
+        });
+    }
+
+ 
     setListener = () => {
-        document.getElementById('next-button').addEventListener('click', this.refresh);
+        var button = document.getElementById('next-button');
+        button.addEventListener('click', this.refresh);
     }
     removeListeners = () => {
         document.getElementById('next-button').removeEventListener('click', this.refresh) ;
     }
+
+    newMethod() {
+        return 'input';
+    }
 }
 
-new AppForm();
+$(document).ready(function() {
+    new AppForm();
+});

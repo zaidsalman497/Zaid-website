83476389af638608e722eb5bffbf70f55c3cd68c
Zaid Salman <58643601+zaidsalman497@users.noreply.github.com>
2020-09-24 13:06:51 +1000

    next button not working

--- loginJS/login.js
+++ loginJS/login.js
@@ -6,22 +6,70 @@ class AppForm {
 
         this.setListener();
         this.getForm();
+        document.getElementById('next-button').disabled = true;
         this.refresh();
+        this.check();
     }
 
+    submit = () => console.log('SUBMIT')
+
+    currentInput = () => this.form[this.step - 1].input;
+    previousInput = () => this.form[this.step - 2].input;
+        
+    check = () => this.currentInput().addEventListener('keyup', this.enableDisable);
+
+    enableDisable = () => {
+        if(this.valid(this.currentInput())) {
+            this.currentInput().classList.remove('invalid');
+            document.getElementById('next-button').disabled = false;
+        }   else {
+            this.currentInput().classList.add('invalid');
+            this.removeListeners();
+            document.getElementById('next-button').disabled = true;
+        }
+    }
+
+    valid = (input) => {
+        let formType = input.id;
+        let value = input.value;
+        let empty = (str) => !str.split('').every(_char => _char !== ' ');
+
+        if(!value || empty(value)) return false
+
+        switch(formType) {
+                     case 'email-input':
+                     return /\S+@\S+\.\S+/.test(value)
+
+                    case 'email-verification-input':
+                    return this.previousInput.value === value;
+
+                    case 'password-input':
+                    return /(?=.*\d)(?=.*[a-z](?=.*[A-Z])[a-zA-Z0-9@$&!]/.test(value)
+
+                    case 'password-verification-input':
+                        return this.previousInput.value === value;;
+
+                        default:
+                            return false;
+
+                    
+        }
+    };
+    
     refresh = () => {
         this.step++;
+        if(this.step <= this.form.length)
         this.displayStep();
+        else
+            this.submit();
     }
-
     displayStep = () => {
-        if(this.currentGroup) { 
-            this.currentGroup.element.style.display = 'none';
-        }
-        
-        this.currentGroup = this.form.find(_group => _group.step === this.step);
-        this.currentGroup.element.style.display = 'block'
+        if(this.currentGroup)
+        this.currentGroup.style.element.display = 'none'
+        this.currentGroup = this.form.find(_group => _group.step).element;
+        this.currentGroup.style.element.display = 'block'
     }
+    
 
     getForm = () => {
         const groups = Array.from(document.getElementsByClassName('form-group'));
@@ -35,7 +83,7 @@ class AppForm {
         });
     }
 
- 
+
     setListener = () => {
         var button = document.getElementById('next-button');
         button.addEventListener('click', this.refresh);
@@ -47,7 +95,7 @@ class AppForm {
     newMethod() {
         return 'input';
     }
-}
+  }
 
 $(document).ready(function() {
     new AppForm();
